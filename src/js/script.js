cc5ab36158c4465367ac7d9ed2674f26e6fb2043
Patryk Pluta <patryk_pluta@interia.pl>
2022-01-22 14:03:59 +0100

    zdjecia

--- src/js/script.js
+++ src/js/script.js
@@ -87,6 +87,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -151,17 +152,23 @@
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
+          if(optionSelected) {
           // check if the option is not default
-           if(!option.default == true) {
+            if(!option.default == true) {
             // add option price to price variable
               price = price + option.price;}
           } 
           // check if the option is default
           else if(!option.default == false) {
             // reduce price variable
-           price = price - option.price;
+            price = price - option.price;          
+          }
+          if(optionImage){
+            if(optionSelected) {optionImage.classList.add(classNames.menuProduct.imageVisible);}
+            else {optionImage.classList.remove(classNames.menuProduct.imageVisible);}
           }
         }
       }
